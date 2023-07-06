ef2828a3447e5000de1b9c17aa3f4f63c76fcf3f
Rafael Lefever <rafael.lefever@discomco.pl>
2023-07-06 11:57:47 +0200

    added run-minimal-win.sh

--- run-minimal-win.sh
+++ run-minimal-win.sh
@@ -2,68 +2,29 @@
 
 
 ## CLEAR ALL DATA
-rm -rf /volume
-
-# WIRESHARK
-mkdir -p /volume/wireshark/config
-
-# FREEIPA
- mkdir -p /volume/freeipa/data
-
-# REDIS
- mkdir -p /volume/redis/data
-
-# RABBITMQQ
- mkdir -p /volume/rabbitmq/data
- mkdir -p /volume/rabbitmq/logs
-
-# MONGODB
- mkdir -p /volume/mongodb/data
+rm -rf /c/volume
 
 # COUCHDB
- mkdir -p /volume/couchdb/log
- mkdir -p /volume/couchdb/data
- mkdir -p /volume/couchdb/config
-
-# EVENTSTORE
- mkdir -p /volume/eventstore/logs
- mkdir -p /volume/eventstore/data
- mkdir -p /volume/eventstore/index
-
-#  mkdir -p /volume/elastic/data
- mkdir -p /volume/elastic/data01
- mkdir -p /volume/elastic/data02
- mkdir -p /volume/elastic/data03
+ mkdir -p /c/volume/couchdb/log
+ mkdir -p /c/volume/couchdb/data
+ mkdir -p /c/volume/couchdb/config
 
 # NATS
- mkdir -p /volume/nats/stream01
-
-# POSTGRESQL
- mkdir -p /volume/postgres/data
-
-# SPACEDECK
- mkdir -p /volume/spacedeck/storage
- mkdir -p /volume/spacedeck/db
-
-# KAFKA
- mkdir -p /volume/kafka/data
-
-# COCKROACHDB
- mkdir -p /volume/crdb/data
+ mkdir -p /c/volume/nats/stream01
 
 
-#  chown $USER -R /volume/
-#  chown 1001 -R /volume/mongodb  # https://hub.docker.com/_/mongo
+#  chown $USER -R /c/volume/
+#  chown 1001 -R /c/volume/mongodb  # https://hub.docker.com/_/mongo
 
 
 git submodule update --remote
 
-docker-compose -f couchdb.yml \
+docker-compose -f couchdb-win.yml \
                -f nats.yml \
                -f networks.yml \
                down
 
-docker-compose -f couchdb.yml \
+docker-compose -f couchdb-win.yml \
                -f nats.yml \
                -f networks.yml \
                up --build $1 
